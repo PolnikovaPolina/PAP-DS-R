6657b47f29bf686a99d281637c7822ea23f4a74f
Polnikova Polina <polnikovapa@gmail.com>
2023-12-21 06:23:57 +0200

    TASK:011:remark_project3

--- project3/run_analysis.R
+++ project3/run_analysis.R
@@ -1,60 +1,61 @@
-# Задає вектор пакетів, які будуть використовуватися в коді
+# Sets the vector of packages to be used in the code
 packages <- c("data.table", "reshape2")
 
-# Завантажує пакети, перевіряючи їх наявність
+# Loads packages, checking for their presence
 sapply(packages, require, character.only=TRUE, quietly=TRUE)
 
-# Задає шлях робочої директорії
-path <- getwd("D:/КПІ/3-курс/Data Science/3 курс/4 тиждень")
+# Sets the working directory path
+path <- "D:/КПІ/3-курс/Data Science/3 курс/4 тиждень"
+setwd(path)
 
-# Задає URL для завантаження даних
+# Sets the URL for downloading the data
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-# Завантажує файл із вказаного URL та зберігає його у заданому шляху
+# Downloads the file from the specified URL and saves it at the given path
 download.file(url, file.path(path, "dataFiles.zip"))
 
-# Розпаковує завантажений архів
+# Unzips the downloaded archive
 unzip(zipfile = "dataFiles.zip")
 
-# Зчитує дані міток активностей та ознак
+# Reads activity labels and feature data
 activityLabels <- fread(file.path(path, "UCI HAR Dataset/activity_labels.txt"), col.names = c("classLabels", "activityName"))
 features <- fread(file.path(path, "UCI HAR Dataset/features.txt") , col.names = c("index", "featureNames"))
 
-# Відбирає ознаки, які мають "mean" або "std" у їхніх назвах
+# Selects features with "mean" or "std" in their names
 featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
 measurements <- features[featuresWanted, featureNames]
 
-# Видаляє круглі дужки з назв ознак
+# Removes parentheses from feature names
 measurements <- gsub('[()]', '', measurements)
 
-# Зчитує та обробляє навчальні дані
+# Reads and processes training data
 train <- fread(file.path(path, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
 data.table::setnames(train, colnames(train), measurements)
 trainActivities <- fread(file.path(path, "UCI HAR Dataset/train/Y_train.txt"), col.names = c("Activity"))
 trainSubjects <- fread(file.path(path, "UCI HAR Dataset/train/subject_train.txt") , col.names = c("SubjectNum"))
 train <- cbind(trainSubjects, trainActivities, train)
 
-# Зчитує та обробляє тестові дані
+# Reads and processes test data
 test <- fread(file.path(path, "UCI HAR Dataset/test/X_test.txt"))[, featuresWanted, with = FALSE]
 data.table::setnames(test, colnames(test), measurements)
 testActivities <- fread(file.path(path, "UCI HAR Dataset/test/Y_test.txt") , col.names = c("Activity"))
 testSubjects <- fread(file.path(path, "UCI HAR Dataset/test/subject_test.txt")   , col.names = c("SubjectNum"))
 test <- cbind(testSubjects, testActivities, test)
 
-# Об'єднує дані навчання та тестування
+# Combines training and test data
 combined <- rbind(train, test)
 
-# Замінює числові мітки активностей на їх текстовий еквівалент
+# Replaces numeric activity labels with their text equivalent
 combined[["Activity"]] <- factor(combined[, Activity], levels = activityLabels[["classLabels"]]   , labels = activityLabels[["activityName"]])
 
-# Змінює номери суб'єктів на фактор
+# Converts subject numbers to factors
 combined[["SubjectNum"]] <- as.factor(combined[, SubjectNum])
 
-# Перетворює дані у довжинний формат
+# Transforms data into long format
 combined <- reshape2::melt(data = combined, id = c("SubjectNum", "Activity"))
 
-# Створює широкий формат, обчислюючи середні значення
+# Creates wide format, computing mean values
 combined <- reshape2::dcast(data = combined, SubjectNum + Activity ~ variable, fun.aggregate = mean)
 
-# Записує оброблені дані у файл "tidyData.txt" у форматі CSV
+# Writes processed data to the "tidyData.txt" file in CSV format
 data.table::fwrite(x = combined, file = "tidyData.txt", quote = FALSE)
